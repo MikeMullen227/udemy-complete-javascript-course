55617f363046b0cea7512efcd68be07171206b7a
Mike Mullen <mmullen227@gmail.com>
2017-03-09 13:51:19 -0500

    pig game up toImplementing Our Hold Function

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -14,13 +14,73 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
+
+//using getElementById because it can be used when IDs are selected and its faster than querySelector
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+//function btn() {
+   //do something here
+//}
+
+//can create the function outside the event handler i.e. the btn function above and pass it into the addEventListener. Known as a callback function. 
+//document.querySelector('.btn-roll').addEventListener('click', btn);
+ 
+//Or the function can be created inside the event listener as an anonomus function 
+document.querySelector('.btn-roll').addEventListener('click', function(){
+	
+	// 1. Random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	// 2. Display the result.
+	var diceDOM = document.querySelector('.dice')
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';	
+	// 3. Update the round score If the rolled number was NOT a 1.
+	if(dice !== 1) {
+		// Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		// next player
+
+		// ternary operator
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		// if activePlayer equals player 0 then activePlayer becomes player 1, else activePlayer becomes player 0.
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		
+		//toggle is better than remove and add in this instance
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		//document.querySelector('player-0-panel').classList.remove('active');
+		//document.querySelector('player-1-panel').classList.add('active');
+
+		document.querySelector('.dice').style.display = 'none';
+
+
+
+	}
+
+
+});
+
+//in this case anonymous function is the right solution because you only want this to happen for just this button.
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#score-0').textContent;
