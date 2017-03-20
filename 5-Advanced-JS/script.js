479c66925daa30d90e9569f02090623d2f4052cb
Mike Mullen <mmullen227@gmail.com>
2017-03-20 16:05:20 -0400

    started coding challenge 4 up to question 4

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -179,10 +179,10 @@ designerQuestion('Jane');
 
 interviewQuestion('teacher')('Mark');	
 
-*/
 
 // Lecture: IIFE (Immediatley Invoked Function Expressions)
 
+/*
 function game() {
 	var score = Math.random() * 10;
 	console.log(score >= 5);
@@ -200,7 +200,164 @@ game();
 	var score = Math.random() * 10;
 	console.log(score >= 5 - goodLuck);
 })(5);
- 
+ */
+
+
+ // Lecture: Closures
+
+/*
+ function retirement(retirementAge) {
+ 	var a = ' years left until retirement.';
+ 	return function(yearOfBirth) {
+ 		var age = 2016 - yearOfBirth;
+ 		console.log((retirementAge - age) + a);
+ 	}
+ }
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+
+//retirement(66)(1990);
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+
+/*
+function interviewQuestion(job) {
+	if (job === 'designer') {
+		return function(name) {
+			console.log(name + ', can you explain what UX design is?');
+		} 
+	} else if (job === 'teacher') {
+		return function(name) {
+			console.log('What subject do you teach ' + name + '?');
+		} 
+	} else {
+			return function(name) {
+				console.log('Hello ' + name + ', What do you do?')
+			}
+	}
+}
+*/
+
+/*
+function interviewQuestion(job) {
+	return function (name) {
+		if (job === 'designer') {
+			console.log(name + ', can you explain what UX design is?'); 
+	} else if (job === 'teacher') {
+			console.log('What subject do you teach ' + name + '?');
+	} else {
+			console.log('Hello ' + name + ', What do you do?')
+	}
+	}
+}
+
+ interviewQuestion('designer')('John');
+*/
+
+
+// Lecture: Bind, Call and Apply
+
+/*
+var john = {
+	name: 'John',
+	age: 26,
+	job: 'teacher',
+	presentation: function(style, timeOfDay) {
+		if (style === 'formal') {
+			console.log('Good ' + timeOfDay + ' ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+		} else if (style === 'friendly') {
+			console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+		}
+	}
+};
+
+var emily = {
+	name: 'Emily',
+	age: 35,
+	job: 'designer'
+
+};
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+
+emilyFormal('afternoon');
+
+
+
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+	var arrRes = [];
+	for(var i = 0; i < arr.length; i++) {
+		arrRes.push(fn(arr[i]));
+	}
+	return arrRes;
+}
+
+function isFullAge(el) {
+	return el >= 18;
+}
+
+function calculateAge(el) {
+	return 2016 - el;
+}
+
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20)) {
+console.log(ages);
+console.log(fullJapan);
+}
+
+
+*/
+
+
+//// Coding Challenge 4
+
+
+var Question = function (question, answers, correctAnswer){
+	this.question = question;
+	this.answers = answers;
+	this.correctAnswer = correctAnswer;
+}
+
+Question.prototype.displayQuestion() {
+	console.log(this.question);
+}
+	 
+
+var question1 = new Question('How far is the sun from the earth in miles?', [93000000, 65000000, 22000000], 0);
+var question2 = new Question('What is the capital of the United States?', ['Los Angeles', 'New York', 'Washington D.C.'], 2);
+var question3 = new Question('What percentage of the human body is made up of water?', [55, 78, 90], 1);
+
+var questions = [question1, question2, question3];
+
+var randomQuestion = questions[Math.floor(Math.random()*questions.length)]
+
+//var randomQuestion = questions[Math.floor(Math.random()*questions.length)]
+//console.log(randomQuestion);
+
+
+
+
+
 
 
 
