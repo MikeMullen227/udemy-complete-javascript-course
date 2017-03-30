eacf7eb7aa9d93074cdf6c661e22998cf641b54b
Mike Mullen <mmullen227@gmail.com>
2017-03-29 22:54:46 -0400

    finished updating budget in UI

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -118,7 +118,7 @@ var UIController = (function() {
 		incomeContainer: '.income__list',
 		expensesContainer: '.expenses__list',
 		budgetLabel: '.budget__value',
-		incomeLabel: '.budget__incom--value',
+		incomeLabel: '.budget__income--value',
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage'
 	}
@@ -173,21 +173,19 @@ var UIController = (function() {
 			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
 			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
 			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
-			document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+			
 
-
-			budget: data.budget,
-				totalInc: data.totals.inc,
-				totalExp: data.totals.exp,
-				percentage: data.percentage
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
 		},
 
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
 	};
-
-
 
 })();
 
@@ -219,7 +217,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		var budget = budgetCtrl.getBudget();
 
 		// 3. Display the budget on the UI
-		console.log(budget);
+		UICtrl.displayBudget(budget);
 	};
 
 
@@ -250,9 +248,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 	return {
 		init: function() {
 			console.log('Application has started.');
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			setupEventListeners();
 		}
-	}
+	};
 	
 
 })(budgetController, UIController);
